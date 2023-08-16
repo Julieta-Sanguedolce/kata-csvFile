52080467fd196c3e99a193caeea0016c28a7b337
Julieta-Sanguedolce <julieta.sanguedolce@gmail.com>
2023-08-16 10:54:41 +0100

    created main function to transform to csv file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
-import { sum } from "./skeleton";
+import fs from "fs";
+import { createObjectCsvWriter } from "csv-writer";
 
-// Call your function of interest, here, instead of sum.
-//You can use vscode's "Debug main.ts" launcher to debug, starting here.
+// Read the text file
+fs.readFile("jokes_for_cohort.txt", "utf8", (err, data) => {
+    if (err) {
+        console.error("Error reading file:", err);
+        return;
+    }
 
-const answer = sum(10, 200);
+    // Process the content (splitting lines and creating objects with an index)
+    const lines = data.split("\n\n").map((line) => line.trim());
 
-console.log({ answer });
+    const objects = lines.map((line, index) => ({
+        index: index + 1, // Adding 1 to make the index start from 1
+        content: line,
+    }));
+
+    const csvWriter = createObjectCsvWriter({
+        path: "jokes.csv",
+        header: [
+            { id: "index", title: "Index" },
+            { id: "content", title: "Content" },
+        ],
+    });
+
+    csvWriter
+        .writeRecords(objects)
+        .then(() => {
+            console.log("CSV file written successfully");
+        })
+        .catch((error) => {
+            console.error("Error writing CSV file:", error);
+        });
+});
